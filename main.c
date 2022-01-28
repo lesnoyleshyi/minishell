faa01338c22b6c2970669a72fe02ec283e7663eb
stycho <stycho@student.21-school.ru>
2022-01-28 20:59:36 +0300

    some more functionality - echo $VAR works in simple cases; leaks fixed; it's still test code

--- main.c
+++ main.c
@@ -1,16 +1,25 @@
 
 #include "minishell.h"
 
-int main()
+int main(int argc, char *argv[], char *envp[])
 {
 	char	*usr_inp;
 	char	*cwd;
-//	char	*env_str;
+	char	*env_var_val;
 
+	argc = 0;
+	argv = NULL;
+	envp = NULL;
+//	while (*envp)
+//	{
+//		printf("%s\n", *envp);
+//		envp += 1;
+//	}
+//	printf("\nthat's all\n");
 	while (1)
 	{
 		usr_inp = readline("shalom goi$ ");
-//		env_str = getenv("PATH");
+
 		if (usr_inp == NULL)
 		{
 			printf("Goi has pressed Ctrl-D\n");
@@ -22,11 +31,21 @@ int main()
 			cwd = getcwd(NULL, 1);
 			printf("%s\n", cwd);
 			free(cwd);
+			free(usr_inp);
 			continue ;
 		}
-		printf("Look: goi said \"%s\"\n", usr_inp);
-		if (ft_strncmp(usr_inp, "exit", 4) == 0)
-			break;
+		if (ft_strncmp(usr_inp, "echo $", 6) == 0)
+		{
+			env_var_val = getenv(usr_inp + 6);
+			printf("%s\n", env_var_val);
+		}
+		else
+		{
+			printf("Look: goi said \"%s\"\n", usr_inp);
+			if (ft_strncmp(usr_inp, "exit", 4) == 0)
+				break;
+		}
+		free(usr_inp);
 	}
 	free(usr_inp);
 	return 0;
